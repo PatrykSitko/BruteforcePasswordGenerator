7b5c463b9f4621e843afff891b81e004a25a382c
Patryk Sitko <patryk.sitko.algemeen@gmail.com>
2023-08-26 04:28:37 +0200

    I've changed from if to ternary operator in factorial function of combinationsCalculator.

--- functions/combinationsCalculator.mjs
+++ functions/combinationsCalculator.mjs
@@ -1,11 +1,7 @@
 function factorial(n) {
-    if (n == 0) {
-        return 1;
-    } else {
-        return n * factorial(n - 1);
-    }
+    return n === 0 ? 1 : n * factorial(n - 1);
 }
 
 export default function ammountOfCombinations(ammountOfSymbols, lengthOfRequestedCombination){
-return factorial(ammountOfSymbols) / (factorial(lengthOfRequestedCombination) * factorial(ammountOfSymbols - lengthOfRequestedCombination));
+    return factorial(ammountOfSymbols) / (factorial(lengthOfRequestedCombination) * factorial(ammountOfSymbols - lengthOfRequestedCombination));
 }
