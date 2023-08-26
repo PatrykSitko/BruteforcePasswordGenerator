b37cc483abed1e7f12ccdab8febbae14175dfc4b
Patryk Sitko <patryk.sitko.algemeen@gmail.com>
2023-08-26 05:16:17 +0200

    Made ammountOfCombinations; commaDigit aware. Uses apropriately ceil or floor.

--- functions/combinationsCalculator.mjs
+++ functions/combinationsCalculator.mjs
@@ -3,5 +3,18 @@ function factorial(n) {
 }
 
 export default function ammountOfCombinations(ammountOfSymbols, lengthOfRequestedCombination){
-    return Math.ceil(factorial(ammountOfSymbols) / (factorial(lengthOfRequestedCombination) * factorial(ammountOfSymbols - lengthOfRequestedCombination)));
+const result = `${factorial(ammountOfSymbols) / (factorial(lengthOfRequestedCombination) * factorial(ammountOfSymbols - lengthOfRequestedCombination))}`;
+    return isCeil(result) ? Math.ceil(Number(result)):Math.floor(result);
+}
+
+function isCeil(digit){
+    if(digit.indexOf(".") === -1){
+        return false;
+    }
+    const afterComaDigit = digit.substring(digit.indexOf(".") + 1, digit.length);
+    const lastAfterComaDigit = `${afterComaDigit}`.substring(afterComaDigit.length - 1, afterComaDigit.length);
+    if(lastAfterComaDigit > 4){
+        return true;
+    }
+    return isCeil(Number(afterComaDigit.substring(0,afterComaDigit.length - 1)));
 }
