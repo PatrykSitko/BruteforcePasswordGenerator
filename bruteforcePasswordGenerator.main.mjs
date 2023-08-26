c31342e93f44791ff4216cf04613ce2cd37c34d4
Patryk Sitko <patryk.sitko.algemeen@gmail.com>
2023-08-26 03:37:26 +0200

    added az09_Combination preset.

--- bruteforcePasswordGenerator.main.mjs
+++ bruteforcePasswordGenerator.main.mjs
@@ -1,2 +1,3 @@
 export { PasswordGenerator } from "./classes/PasswordGenerator.mjs";
 export { default as azAZ09_Combination } from "./combination-presets/a-zA-Z0-9.mjs";
+export { default as az09_Combination } from "./combination-presets/a-z0-9.mjs";
