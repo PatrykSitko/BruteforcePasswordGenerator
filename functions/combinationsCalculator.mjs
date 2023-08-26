9b8bfbcf7b3a5d9064dd2f6378f50ce441eb7488
Patryk Sitko <patryk.sitko.algemeen@gmail.com>
2023-08-26 05:23:01 +0200

    reformatted code.

--- functions/combinationsCalculator.mjs
+++ functions/combinationsCalculator.mjs
@@ -1,20 +1,32 @@
 function factorial(n) {
-    return n === 0 ? 1 : n * factorial(n - 1);
+  return n === 0 ? 1 : n * factorial(n - 1);
 }
 
-export default function ammountOfCombinations(ammountOfSymbols, lengthOfRequestedCombination){
-const result = `${factorial(ammountOfSymbols) / (factorial(lengthOfRequestedCombination) * factorial(ammountOfSymbols - lengthOfRequestedCombination))}`;
-    return isCeil(result) ? Math.ceil(Number(result)):Math.floor(Number(result));
+export default function ammountOfCombinations(
+  ammountOfSymbols,
+  lengthOfRequestedCombination,
+) {
+  const result = `${
+    factorial(ammountOfSymbols) /
+    (factorial(lengthOfRequestedCombination) *
+      factorial(ammountOfSymbols - lengthOfRequestedCombination))
+  }`;
+  return isCeil(result)
+    ? Math.ceil(Number(result))
+    : Math.floor(Number(result));
 }
 
-function isCeil(digit){
-    if(digit.indexOf(".") === -1){
-        return false;
-    }
-    const afterComaDigit = digit.substring(digit.indexOf(".") + 1, digit.length);
-    const lastAfterComaDigit = `${afterComaDigit}`.substring(afterComaDigit.length - 1, afterComaDigit.length);
-    if(lastAfterComaDigit > 4){
-        return true;
-    }
-    return isCeil(Number(afterComaDigit.substring(0,afterComaDigit.length - 1)));
+function isCeil(digit) {
+  if (digit.indexOf(".") === -1) {
+    return false;
+  }
+  const afterComaDigit = digit.substring(digit.indexOf(".") + 1, digit.length);
+  const lastAfterComaDigit = `${afterComaDigit}`.substring(
+    afterComaDigit.length - 1,
+    afterComaDigit.length,
+  );
+  if (lastAfterComaDigit > 4) {
+    return true;
+  }
+  return isCeil(Number(afterComaDigit.substring(0, afterComaDigit.length - 1)));
 }
